067d61ab50e7b67267b1b82d1cfdb03193b7160e
forrestmorrison <forrestmorrison@gmail.com>
2023-01-30 20:07:05 -0600

    setup parks controller

---
+++ controllers/parks.js
@@ -0,0 +1,23 @@
+const parks = require("../data/parks")
+
+const listParks = () => {
+}
+
+const showPark = () => {
+
+}
+
+const createPark = () => {
+
+}
+
+const deletePark = () => {
+
+}
+
+module.exports = {
+    listParks,
+    showPark,
+    createPark,
+    deletePark
+}
