1cb43e234e9d0114305c9bb3aa90a6365ea04b25
forrestmorrison <forrestmorrison@gmail.com>
2023-01-30 19:20:32 -0600

    set up parks data and router files

---
+++ data/parks.js
@@ -0,0 +1,23 @@
+exports.parks = [
+    {
+        "_id": 1,
+        "name": "Rockstar Energy Bike Park",
+        "address": "12257 Kuykendahl Rd, Houston, TX 77067",
+        "type": "dirt, race, street",
+        "access": "free"
+    },
+    {
+        "_id": 2,
+        "name": "9th Street BMX Park",
+        "address": "900 9th St, Austin, TX 78703",
+        "type": "dirt",
+        "access": "free"
+    },
+    {
+        "_id": 3,
+        "name": "Capitol City BMX",
+        "address": "5001 Killingsworth Ln, Pflugerville, TX 78660",
+        "type": "race",
+        "access": "paid"
+    }
+]
---
+++ routes/parks.js
@@ -0,0 +1,5 @@
+const express = require("express")
+const router = express.Router()
+const parksController = require("../controllers/parks")
+
+module.exports = router
