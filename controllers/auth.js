bf8b63c20b07bad8bc9a3e50b656950e07bab613
forrestmorrison <forrestmorrison@gmail.com>
2023-02-03 21:46:35 -0600

    added auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,67 @@
+const mysql = require('mysql')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const pool = require('../sql/connection')
+const { handleSQLError } = require('../sql/error')
+
+// // for bcrypt
+const saltRounds = 10
+
+const signup = (req, res) => {
+  const { email, name, password } = req.body
+  let sql = "INSERT INTO users (email, name, password) VALUES (?, ?, ?)"
+
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    sql = mysql.format(sql, [ email, hash, name ])
+  
+    pool.query(sql, (err, result) => {
+      console.log('result', result)
+      if (err) {
+        if (err.code === 'ER_DUP_ENTRY') return res.status(409).send('email is already registered')
+        return handleSQLError(res, err)
+      }
+      return loginUser(email, password, res)
+    })
+  })
+}
+
+const loginUser = (email, password, res) => {
+  let sql = "SELECT * FROM users WHERE email = ?"
+  sql = mysql.format(sql, [ email ])
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err)
+    if (!rows.length) return res.status(404).send('No matching users')
+
+    const hash = rows[0].password
+    bcrypt.compare(password, hash)
+      .then(result => {
+        if (!result) return res.status(400).send('Invalid password')
+
+        const data = { ...rows[0] }
+        data.password = 'REDACTED'
+
+        const token = jwt.sign(data, 'secret')
+        res.json({
+          msg: 'Login successful',
+          token,
+          user: data
+        })
+      })
+  })
+}
+
+const login = (req, res) => {
+  const { email, password } = req.body
+  loginUser(email, password, res)
+}
+
+const checkUser = (req, res)  => {
+  res.send(req.user)
+}
+
+module.exports = {
+  signup,
+  login,
+  checkUser
+}
